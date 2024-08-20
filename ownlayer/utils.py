48c17762b6499dc4d89297712b7949d8cf7e6b91
Diego Sisto <41039+suelopoder@users.noreply.github.com>
2024-08-20 11:05:59 -0300

    Incremented minor version

--- ownlayer/utils.py
+++ ownlayer/utils.py
@@ -4,4 +4,4 @@ def now():
     return int(datetime.now(timezone.utc).timestamp() * 1000)
 
 def get_metadata():
-    return { "_source": "ownlayer_py_sdk", "_sdk_version": "0.1.1" }
+    return { "_source": "ownlayer_py_sdk", "_sdk_version": "0.2.0" }
